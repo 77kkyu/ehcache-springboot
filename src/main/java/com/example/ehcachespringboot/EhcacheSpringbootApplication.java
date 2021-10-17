e90045baf378fc2c535aca4e5ff7cf27c0df6722
77kkyu <77kkyu@gmail.com>
2021-10-17 15:50:17 +0900

    feat setting

---
+++ src/main/java/com/example/ehcachespringboot/EhcacheSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.ehcachespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhcacheSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhcacheSpringbootApplication.class, args);
+	}
+
+}
